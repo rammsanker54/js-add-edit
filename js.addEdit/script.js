38d6d7537968822c1b3d44b05ef42efd994ac374
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:52:17 +0530

    Add files via upload

---
+++ js.addEdit/script.js
@@ -0,0 +1,24 @@
+// function op(){
+
+// var nn=document.getElementById("num").value;
+// var y=document.createElement(li).innerHTML=nn;
+
+// document.getElementById("input").innerHTML=y;
+// append.child(y)
+
+
+// }
+// document.addEventListener('click' ,op)
+function todo(){
+    var x=document.getElementById("num").value;
+    var y=document.getElementById("demo");
+    var h=document.createElement("h2");
+   
+    h.innerText=x;
+    y.appendChild(h);
+}
+function todel(){
+    var z=document.getElementById("del");
+    var r=document.getElementById("demo")  ;
+    r.delete
+}
